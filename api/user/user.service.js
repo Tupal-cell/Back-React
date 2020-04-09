beb2d395553430c33478b0e484de427612d2c250
Tupal-cell <55320896+Tupal-cell@users.noreply.github.com>
2020-04-09 16:33:22 +0200

    Update user.service.js

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -3,7 +3,21 @@ const pool = require ("../../config/database");
 module.exports = {
     create: (data, callBack) => {
         pool.query(
-            `INSERT INTO utilisateur (prenom, nom, sexe, email, password, tel)VALUES(?, ?, ?, ?, ?, ?)` [data.firstName, data.lastName, data.gender, data.email, data.passWord, data.]
-            );
+            `INSERT INTO utilisateur (prenom, nom, sexe, email, password, tel)VALUES(?, ?, ?, ?, ?, ?)` 
+            [
+                data.firstName, 
+                data.lastName, 
+                data.gender, 
+                data.email, 
+                data.passWord, 
+                data.tel
+            ],
+            (error,results,fields) => {
+                if(error) {
+                    return callBack(error);
+                }
+                    return callBack(null, results);
+            
+        );
     },
 };
